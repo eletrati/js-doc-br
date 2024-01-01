8cafd4e11583ea5796d7059c9d4573dec13e28a0
Samuel Godoy <31517839+samgodoy@users.noreply.github.com>
2024-01-01 18:49:20 -0300

    Update cpf.js

--- cpf.js
+++ cpf.js
@@ -1,24 +1,9 @@
 function validarCPF(cpf) {
-    cpf = cpf.replace(/[^\d]/g,''); // remove qualquer caracter que não seja dígito
-    if(cpf === '' || cpf.length !== 11) return false;
-    let cpfNumbers = Array.from(cpf);
-    let sum = 0;
-    for (let i = 0; i < cpfNumbers.length-2; i++) {
-        sum += cpfNumbers[i] * (cpfNumbers.length-1 - i);
-    }
-    let firstDigit = sum % 11;
-    firstDigit = firstDigit < 2 ? 0 : 11 - firstDigit;
-    if (firstDigit !== parseInt(cpfNumbers[9])) {
-        return false;
-    }
-    sum = 0;
-    for (let i = 0; i < cpfNumbers.length-1; i++) {
-        sum += cpfNumbers[i] * (cpfNumbers.length - i);
-    }
-    let secondDigit = sum % 11;
-    secondDigit = secondDigit < 2 ? 0 : 11 - secondDigit;
-    if (secondDigit !== parseInt(cpfNumbers[10])) {
-        return false;
-    }
-    return true;
+    cpf = cpf.replace(/\D/g, ''); 
+    if (/^(\d)\1+$/.test(cpf)) return false;
+    const calculateDigit = (length) => {
+        let sum = Array.from(cpf).map(Number).slice(0, length).reduce((acc, digit, index) => acc + digit * (length + 1 - index), 0);
+        return (sum % 11) < 2 ? 0 : 11 - (sum % 11) === Array.from(cpf).map(Number)[length];
+    };
+    return calculateDigit(9) && calculateDigit(10);
 }
